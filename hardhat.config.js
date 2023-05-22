c7cf8dbbc3c294d236bba918f8683a48238bf6dc
DanielDimitrov5 <danieldimitrov2304@gmail.com>
2023-05-22 13:56:56 +0300

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,18 +7,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  solidity: "0.8.17",
-  networks: {
-    goerli: {
-        // url: 'http://127.0.0.1:8545/',
-        url: process.env.RPCURL,
-        // accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
-        accounts: [process.env.PRIVATE_KEY]
+    solidity: "0.8.17",
+    networks: {
+        goerli: {
+            url: process.env.RPCURL,
+            accounts: [process.env.PRIVATE_KEY]
+        },
     },
-  },
-  etherscan: {
-    apiKey: {
-      goerli: process.env.ETHERSCAN_API_KEY
+    etherscan: {
+        apiKey: {
+            goerli: process.env.ETHERSCAN_API_KEY
+        }
+    },
+    settings: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
     }
-  }
 };
